2a91290a7d720fced8c48b1741c62277d3bf5785
Cameron <camcreel@gmail.com>
2021-09-02 18:31:57 -0400

    Completed assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,6 +14,8 @@ function MovieForm() {
     female_director: false,
   });
 
+  const [errors, setErrors] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -22,10 +24,33 @@ function MovieForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
   }
+
+  // async function handleSubmit(e) {
+  //   e.preventDefault();
+  //   // fetch returns a Promise, we must await it
+  //   const response = await fetch("/movies", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   });
+  //   // response.json() returns a Promise, we must await it
+  //   const data = await response.json();
+  //   if (response.ok) {
+  //     console.log("Movie created:", data);
+  //   } else {
+  //     setErrors(data.errors);
+  //   }
+  // }
 
   function handleChange(e) {
     const value =
@@ -125,6 +150,15 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {
+          errors.length > 0 && (
+            <ul style={{ color: "red" }}>
+              {errors.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )
+        }
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
